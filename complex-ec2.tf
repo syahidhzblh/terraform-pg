6ff8659e9032a67f548971efb5f7d254c6bc988f
syahidhzblh <syahidhizbullah17@gmail.com>
2023-10-31 16:18:20 +0700

    add:complex-ec2.tf

---
+++ complex-ec2.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.ec2-attach-eip.id
+  allocation_id = aws_eip.lb.id
+}
+
+resource "aws_eip" "lb" {
+  domain = "vpc"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH for Instance"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+#Instance Argument
+resource "aws_instance" "ec2-attach-eip" {
+  ami                    = var.ami_ubuntu
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name               = "server-keypair"
+
+  tags = {
+    Name = "ubuntu-terraform-eip"
+  }
+}
+
+
+#Output Value
+output "public_ip" {
+  value = aws_eip.lb.public_ip
+}
+
+output "instance_public_ip" {
+  value = aws_instance.ec2-attach-eip.public_ip
+}
+
+output "vpc_security_group_ids" {
+  value = aws_security_group.allow_tls.id
+}
+
+output "instance_security_group" {
+  value = aws_instance.ec2-attach-eip.security_groups
+}
